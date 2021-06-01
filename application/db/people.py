e2aa2fdf2b25c19dbcbf702c5c3f3a67d5762b32
Daria Narezhneva <103203@gmail.com>
2021-06-01 21:00:00 +0300

    Add application

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+
+def get_employees():
+    print("get employees function", datetime.date(datetime.now()), datetime.time(datetime.now()))
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+
+def calculate_salary():
+    print("calculate salary function", datetime.today())
